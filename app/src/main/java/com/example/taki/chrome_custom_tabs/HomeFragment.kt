d953d8d89d946f94016094d272c78dfe1eee5ebd
taki4227 <takiki.truth@gmail.com>
2020-10-22 16:56:07 +0900

    Migrate navigation component

---
+++ app/src/main/java/com/example/taki/chrome_custom_tabs/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.taki.chrome_custom_tabs
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.taki.chrome_custom_tabs.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.simpleButton.setOnClickListener {
+            requireActivity().startActivity(
+                Intent(
+                    requireActivity(),
+                    SimpleCustomTabActivity::class.java
+                )
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/taki/chrome_custom_tabs/MainActivity.kt
+++ app/src/main/java/com/example/taki/chrome_custom_tabs/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.taki.chrome_custom_tabs
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.taki.chrome_custom_tabs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.simple_button).setOnClickListener {
-            startActivity(Intent(this, SimpleCustomTabActivity::class.java))
-        }
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
